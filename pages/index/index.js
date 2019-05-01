404dfdae273ced4c3e84166a8d775f54a4812b7d
panhe-xue <1158817942@qq.com>
2019-05-01 10:32:08 +0800

    a

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,78 +4,21 @@ const app = getApp()
 
 Page({
   data: {
-    width: 0,
-    height: 0,
     currentPageIndex: 0,
-    index: 4,
-    top: 0,
-    lastX: 0,
+    totalIndex: 4,
     istop: 'stop',
-    lastY: 0,
-    isTransition: false,
-    animation: ''
+    startY: 0,
+    endY: 0,
+    margintop: 0
   },
   onReady() {
     this.audioCtx = wx.createInnerAudioContext("myAudio")
   },
-  handltouchstart(event) {
-    this.data.lastX = event.touches[0].pageX
-    this.data.lastY = event.touches[0].pageY
-  },
-  handletouchmove(event) {
-    if(this.data.isTransition) return
-    let currentX = event.touches[0].pageX
-    let currentY = event.touches[0].pageY
-    let tx = currentX - this.data.lastX
-    let ty = currentY - this.data.lastY
-    //上下方向滑动
-    if (ty < 0)
-      this.pullDown()
-    else if (ty > 0)
-      this.pullUp()
-    //将当前坐标进行保存以进行下一次计算
-    this.data.lastX = currentX
-    this.data.lastY = currentY
-  },
-  pullDown(event) {
-    const that = this
-    if(this.data.isTransition) return
-    if(that.data.currentPageIndex >= that.data.index || that.data.currentPageIndex < 0) return
-    this.setData({
-      isTransition: true
-    })
-    this.setData({
-      currentPageIndex: that.data.currentPageIndex+1
-    })
-    that.doAnimation()
-  },
-  pullUp() {
-    const that = this
-    if(that.data.currentPageIndex > that.data.index || that.data.currentPageIndex <= 0) return
-    this.setData({
-      isTransition: true
-    })
-    this.setData({
-      currentPageIndex: that.data.currentPageIndex-1
-    })
-    that.doAnimation()
-  },
-  doAnimation() {
-    const that = this
-    this.setData({
-      top: -(that.data.height * that.data.currentPageIndex) + 'px'
-    })
-  },
-  onLoad: function () {
-    this.setData({
-      width: +app.globalData.screenWidth,
-      height: +app.globalData.screenHeight
-    })
-  },
-  transitionEnd: function () {
-    this.setData({
-      isTransition: false
-    })
+  doTouchstart() {},
+  doTouchmove() {},
+  doTouchend() {},
+  pullDown() {
+    console.log("pulldown")
   },
   dostop() {
     const that = this
