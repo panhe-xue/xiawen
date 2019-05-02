f31ec6373d1f611d6e7bb0de50fc3b06c05a39ae
panhe.com <1158817942@qq.com>
2019-05-02 20:40:23 +0800

    a

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,13 +12,41 @@ Page({
     margintop: 0
   },
   onReady() {
-    this.audioCtx = wx.createInnerAudioContext("myAudio")
+    this.innerAudioContext = wx.createInnerAudioContext()
+    this.innerAudioContext.src = "https://182.61.38.185:8001/music.mp3"
+    this.innerAudioContext.autoplay = false
+    this.innerAudioContext.loop = true
   },
-  doTouchstart() {},
-  doTouchmove() {},
-  doTouchend() {},
+  doTouchstart(e) {
+    const pointY = e.touches[0].pageY
+    this.setData({
+      startY: pointY
+    })
+  },
+  doTouchmove(e) {
+    const pointY = e.touches[0].pageY
+    const d = this.data
+    this.setData({
+      endY: pointY,
+      margintop: d.endY - d.startY
+    })
+  },
+  doTouchend(e) {
+    const that = this
+    if(this.data.endY - this.data.startY > 0 && Math.abs(this.data.margintop) > 100) {
+      this.data.currentPageIndex > 0 ? this.setData({currentPageIndex: that.data.currentPageIndex-1}) : ''
+    } else if(Math.abs(this.data.margintop) > 100) {
+      this.data.currentPageIndex < this.data.totalIndex ? this.setData({currentPageIndex: that.data.currentPageIndex+1}) : ''
+    }
+    this.setData({
+      startY: 0,
+      endY: 0,
+      margintop: 0
+    })
+  },
   pullDown() {
-    console.log("pulldown")
+    const that = this
+    this.data.currentPageIndex < this.data.totalIndex ? this.setData({currentPageIndex: that.data.currentPageIndex+1}) : ''
   },
   dostop() {
     const that = this
@@ -28,6 +56,6 @@ Page({
     this.stopMusic()
   },
   stopMusic() {
-    this.data.istop == 'stop' ? this.audioCtx.play() : this.audioCtx.stop()
+    this.data.istop == 'stop' ? this.innerAudioContext.play() : this.innerAudioContext.pause()
   }
 })
